9b287a00b9e4a72e6b98a1af9622107e5a8e168c
Felicia Wold <feliciawold@gmail.com>
2019-02-11 17:29:56 -0700

    created weightedLottery.js updated bootstrap file

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,2 +1,11 @@
 // ES6 code goes here
 console.log('es6 code goes here');
+
+import weightedLottery from './weightedLottery';
+const weights2 = {
+    green: 1,
+    yellow: 2,
+    red: 3
+  };
+
+console.log(weightedLottery(weights2));
---
+++ src/weightedLottery.js
@@ -0,0 +1,31 @@
+const weightedLottery = weights => {
+   // keep track of your  weights
+   // [‘green’, ‘yellow’, ‘yellow’, ‘red’, ‘red’, ‘red’]
+   let picker = [];
+
+   Object.keys(weights).forEach(key =>{
+       for (let i = 0; i < weights[key]; i++) {
+           picker.push(key);
+       }
+   });
+
+   return picker[(Math.random() * picker.length) | 0];
+   // return picker[Math.floor((Math.random() * picker.length))];
+}
+
+const weights = {
+   winning: 1,
+   losing: 1000
+}
+
+const weights2 = {
+   green: 1,
+   yellow: 2,
+   red: 3
+ };
+
+weightedLottery(weights)
+weightedLottery(weights2)
+
+console.log(weightedLottery(weights));
+console.log(weightedLottery(weights2));
